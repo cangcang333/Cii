79ec8e9df5037e558d79115feb49a0f32a20bcc7
Danial <cangcang333@163.com>
2021-09-30 20:44:35 +0800

    mem, fmt code

--- src/mem.c
+++ src/mem.c
@@ -25,6 +25,41 @@ void *Mem_alloc(long nbytes, const char *file, int line) {
     return ptr;
 }
 
+void *Mem_calloc(long count, long nbytes, const char *file, int line) {
+    void *ptr;
+    assert(count > 0);
+    assert(nbytes > 0);
+    ptr = calloc(count, nbytes);
+    if (ptr == NULL) {
+        if (file == NULL) {
+            RAISE(Mem_Failed);
+        } else {
+            Except_raise(&Mem_Failed, file, line);
+        }
+    }
+}
+
+void Mem_free(void *ptr, const char *file, int line) {
+    if (ptr) {
+        free(ptr);
+    }
+}
+
+void *Mem_resize(void *ptr, long nbytes, const char *file, int line) {
+    assert(ptr);
+    assert(nbytes > 0);
+    ptr = realloc(ptr, nbytes);
+    if (ptr == NULL) {
+        if (file == NULL) {
+            RAISE(Mem_Failed);
+        } else {
+            Except_raise(&Mem_Failed, file, line);
+        }
+    }
+
+    return ptr;
+}
+
 
 void test_api(char *msg) {
     printf("test_api msg: %s\n", msg);
